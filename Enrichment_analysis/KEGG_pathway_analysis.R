a5922fcd742cfc427f12ece79f5a0d4febeb7b80
IbrahimElzahaby <ibrahimmohsen342@gmail.com>
2023-05-25 13:13:26 +0200

    update documentation

--- Enrichment_analysis/KEGG_pathway_analysis.R
+++ Enrichment_analysis/KEGG_pathway_analysis.R
@@ -1,75 +1,163 @@
+
+# Load required libraries
 library(readxl)
 library(clusterProfiler)
 library(enrichplot)
 library(DOSE)
 library(org.Hs.eg.db)
 library(pathfindR)
-BiocManager::install("clusterProfiler", version = "3.16")
-BiocManager::install("pathview")
-BiocManager::install("enrichplot")
+
+# Read Hela gene IDs data from Excel file
 hela_genes <- read_excel("hela_idmap.xlsx")
+
+# Read PDAC gene IDs data from Excel file
 pdac <- read_excel("idmap.xlsx")
+
+# Read Melanoma gene IDs data from Excel file
 melanoma_genes <- read_excel("melanoma_idmap.xlsx")
+
+# Read Monocytes gene IDs data from Excel file
 monocytes_genes <- read_excel("monocytes_idmap.xlsx")
-# Hela_KEGG pathways
+
+################################################################################
+
+# Perform KEGG pathway enrichment analysis for Hela cells
 hela_kegg <- enrichKEGG(gene = hela_genes$entrezgene,
                         organism = 'hsa',
-                        pvalueCutoff = 0.05, qvalueCutoff = 0.3, keyType = "kegg")
+                        pvalueCutoff = 0.05, 
+                        qvalueCutoff = 0.3, 
+                        keyType = "kegg")
+
+# Generate a dotplot to visualize the enriched pathways for Hela cells
 hela_kegg_dot <- dotplot(hela_kegg)
+
+# Calculate pairwise similarity between enriched terms
 he <- pairwise_termsim(hela_kegg)
+
+# Generate an enriched map plot to visualize the connectivity between enriched terms
 he_map <- emapplot(he, showCategory=10)
-hecenter <- setReadable(hela_kegg, OrgDb = "org.Hs.eg.db", keyType = "ENTREZID")
-hela_kegg_cnet <- cnetplot(hecenter,  foldChange=hela_kegg, circular = F, colorEdge = TRUE, node_lable="all", showCategory = 5)
-# PDAC KEGG pathways
+
+# Convert the gene IDs in the enriched pathways to readable format using the org.Hs.eg.db package
+hecenter <- setReadable(hela_kegg, OrgDb = "org.Hs.eg.db", 
+                        keyType = "ENTREZID")
+
+# Generate a cnetplot to visualize the enriched pathways for Hela cells
+hela_kegg_cnet <- cnetplot(hecenter,  foldChange=hela_kegg, 
+                           circular = F, colorEdge = TRUE, 
+                           node_lable="all", showCategory = 5)
+
+################################################################################
+
+# Perform KEGG pathway enrichment analysis for PDAC cells
 pdac_kegg <- enrichKEGG(gene = pdac$entrezgene,
                         organism = 'hsa',
-                        pvalueCutoff = 0.05, qvalueCutoff = 0.3)
+                        pvalueCutoff = 0.05, 
+                        qvalueCutoff = 0.3)
+
+# Generate a dotplot to visualize the enriched pathways for PDAC cells
 pdac_kegg_dot <- dotplot(pdac_kegg)
+
+# Calculate pairwise similarity between enriched terms
 pdkegg <- pairwise_termsim(pdac_kegg)
+
+# Generate an enriched map plot to visualize the connectivity between enriched terms
 pd_map <- emapplot(pdkegg, showCategory=10)
-pkcenter <- setReadable(pdac_kegg, OrgDb = "org.Hs.eg.db", keyType = "ENTREZID")
-pdac_kegg_cnet <- cnetplot(pkcenter,  foldChange=pdac_kegg, circular = F, colorEdge = TRUE, node_lable="all", showCategory = 5)
-# melanoma KEGG
+
+# Convert the gene IDs in the enriched pathways to readable format using the org.Hs.eg.db package
+pkcenter <- setReadable(pdac_kegg, OrgDb = "org.Hs.eg.db", 
+                        keyType = "ENTREZID")
+
+# Generate a cnetplot to visualize the enriched pathways for PDAC cells
+pdac_kegg_cnet <- cnetplot(pkcenter,  foldChange=pdac_kegg, 
+                           circular = F, colorEdge = TRUE, 
+                           node_lable="all", showCategory = 5)
+
+################################################################################
+
+# Perform KEGG pathway enrichment analysis for Melanoma cells
 melanoma_kegg <- enrichKEGG(gene = melanoma_genes$entrezgene,
                             organism = 'hsa',
-                            pvalueCutoff = 0.05, qvalueCutoff = 0.3)
+                            pvalueCutoff = 0.05, 
+                            qvalueCutoff = 0.3)
+
+# Generate a dotplot to visualize the enriched pathways for Melanoma cells
 melanoma_kegg_dot <- dotplot(melanoma_kegg)
+
+# Calculate pairwise similarity between enriched terms
 mk <- pairwise_termsim(melanoma_kegg)
+
+# Generate an enriched map plot to visualize the connectivity between enriched terms
 mela_map <- emapplot(mk, showCategory=10)
-mkcenter <- setReadable(melanoma_kegg, OrgDb = "org.Hs.eg.db", keyType = "ENTREZID")
-mela_kegg_cnet <- cnetplot(mkcenter,  foldChange=melanoma_kegg, circular = F, colorEdge = TRUE, node_lable="all", showCategory = 5)
-# monocytes KEGG
+
+# Convert the gene IDs in the enriched pathways to readable format using the org.Hs.eg.db package
+mkcenter <- setReadable(melanoma_kegg, OrgDb = "org.Hs.eg.db", 
+                        keyType = "ENTREZID")
+
+# Generate a cnetplot to visualize the enriched pathways for Melanoma cells
+mela_kegg_cnet <- cnetplot(mkcenter,  foldChange=melanoma_kegg, 
+                           circular = F, colorEdge = TRUE, 
+                           node_lable="all", showCategory = 5)
+
+################################################################################
+
+# Perform KEGG pathway enrichment analysis for Monocytes cells
 monocytes_kegg <- enrichKEGG(gene = monocytes_genes$entrezgene,
                              organism = 'hsa',
-                             pvalueCutoff = 0.05, qvalueCutoff = 0.3)
+                             pvalueCutoff = 0.05, 
+                             qvalueCutoff = 0.3)
+
+# Generate a dotplot to visualize the enriched pathways for Monocytes cells
 monocytes_kegg_dot <- dotplot(monocytes_kegg)
+
+# Calculate pairwise similarity between enriched terms
 mok <- pairwise_termsim(monocytes_kegg)
+
+# Generate an enriched map plot to visualize the connectivity between enriched terms
 mok_emap <- emapplot(mok, showCategory=10)
-mokcenter <- setReadable(monocytes_kegg, OrgDb = "org.Hs.eg.db", keyType = "ENTREZID")
-mono_kegg_cnet <- cnetplot(mokcenter,  foldChange=monocytes_kegg, circular = F, colorEdge = TRUE, node_lable="all", showCategory = 5)
-#####
+
+# Convert the gene IDs in the enriched pathways to readable format using the org.Hs.eg.db package
+mokcenter <- setReadable(monocytes_kegg, OrgDb = "org.Hs.eg.db", 
+                         keyType = "ENTREZID")
+
+# Generate a cnetplot to visualize the enriched pathways for Monocytes cells
+mono_kegg_cnet <- cnetplot(mokcenter,  foldChange=monocytes_kegg, 
+                           circular = F, colorEdge = TRUE, 
+                           node_lable="all", showCategory = 5)
+
+################################################################################
+
 # Draw the dot KEGG diagrams
 res <- 300
 w <- 16
 h <- 12
 png("kegg_dot.png", width = w*res, height = h*res, res = res)
-cowplot::plot_grid(monocytes_kegg_dot, hela_kegg_dot, melanoma_kegg_dot, pdac_kegg_dot, ncol = 2, labels = c('A', 'B', 'C', 'D'), label_size = 24)
+cowplot::plot_grid(monocytes_kegg_dot, hela_kegg_dot, 
+                   melanoma_kegg_dot, pdac_kegg_dot, 
+                   ncol = 2, labels = c('A', 'B', 'C', 'D'), 
+                   label_size = 24)
 dev.off()
+
 #####
+
 # Draw the E-Map KEGG diagrams
 res <- 300
 w <- 16
 h <- 12
 png("kegg_E-Map.png", width = w*res, height = h*res, res = res)
-cowplot::plot_grid(mok_emap, he_map, mela_map, pd_map, ncol = 2, labels = c('A', 'B', 'C', 'D'), label_size = 24)
+cowplot::plot_grid(mok_emap, he_map, mela_map, pd_map, ncol = 2, 
+                   labels = c('A', 'B', 'C', 'D'), label_size = 24)
 dev.off()
+
 #####
+
 # Draw the Cnet KEGG diagrams
 res <- 250
 w <- 20
 h <- 15
 png("kegg_Cnet2.png", width = w*res, height = h*res, res = res)
-cowplot::plot_grid(mono_kegg_cnet, hela_kegg_cnet, mela_kegg_cnet, pdac_kegg_cnet, ncol = 2, labels = c('A', 'B', 'C', 'D'), label_size = 24)
+cowplot::plot_grid(mono_kegg_cnet, hela_kegg_cnet, mela_kegg_cnet, 
+                   pdac_kegg_cnet, ncol = 2, labels = c('A', 'B', 'C', 'D'), 
+                   label_size = 24)
 dev.off()
 
 
